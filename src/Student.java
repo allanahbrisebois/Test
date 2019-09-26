5c6c5e28eda79526b274d2244e48424bf53877b6
lanab <lanab@10.16.46.144>
2019-09-26 19:01:40 -0400

    3rd comment

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,27 @@
 */
 public class Student {
         private String name;
+        
+    private String string = status;
+
+    /**
+     * Get the value of string
+     *
+     * @return the value of string
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Set the value of string
+     *
+     * @param string new value of string
+     */
+    public void setString(String string) {
+        this.string = string;
+    }
+
 
 
     public String getName() {
